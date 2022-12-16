0556a488343b33eb55410ba22cd9af02b40df263
pardha-nolo <113441531+pardha-nolo@users.noreply.github.com>
2022-12-16 21:59:04 +0530

    transaction response change

--- routes/parser.js
+++ routes/parser.js
@@ -1,17 +1,20 @@
 const parser = require('transaction-sms-parser');
 
 
-const parse = function (request){
+const parse = function (request) {
     let message = request.message;
     let transactionInfo = parser.getTransactionInfo(message);
-    if(transactionInfo.account.number == null){
+    if (transactionInfo.account.number == null) {
         let split = message.split(" ");
-        for(i=0; i<split.length; i++){
-            if(split[i].length > 0 && split[i].toLowerCase().includes("xx")){
+        for (i = 0; i < split.length; i++) {
+            if (split[i].length > 0 && split[i].toLowerCase().includes("xx")) {
                 transactionInfo.account.number = split[i];
             }
         }
     }
+    transactionInfo["accountNumber"] = transactionInfo.account.number;
+    transactionInfo["balance"] = transactionInfo.balance.available ? transactionInfo.balance.available : null;
+    delete transactionInfo.account;
     return transactionInfo;
 }
 
