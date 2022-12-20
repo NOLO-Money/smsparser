d1941fcd54930a09595a870b7a152d97acc29fc7
pardha-nolo <113441531+pardha-nolo@users.noreply.github.com>
2022-12-20 12:24:48 +0530

    sbi account and due changes

--- routes/parser.js
+++ routes/parser.js
@@ -7,10 +7,21 @@ const parse = function (request) {
     if (transactionInfo.account.number == null) {
         let split = message.split(" ");
         for (i = 0; i < split.length; i++) {
-            if (split[i].length > 0 && split[i].toLowerCase().includes("xx")) {
-                transactionInfo.account.number = split[i];
+            if (split[i].length > 0 && (split[i].toLowerCase().includes("xx") || split[i].toLowerCase().includes("**"))) {
+                let j = 4;
+                transactionInfo.account.number = split[i]
+                while(j-->0) {
+                    transactionInfo.account.number = transactionInfo.account.number.toLowerCase()
+                        .replace("x", "")
+                        .replace("*", "");
+                }
             }
         }
+    }
+    let message_lower = message.toLowerCase();
+    if(message_lower.includes("cancel") || message_lower.includes("decline") || message_lower.includes("due")){
+        transactionInfo["transactionAmount"] = 0
+        transactionInfo["transactionType"] = "invalid"
     }
     transactionInfo["accountNumber"] = transactionInfo.account.number;
     transactionInfo["balance"] = transactionInfo.balance.available ? transactionInfo.balance.available : null;
