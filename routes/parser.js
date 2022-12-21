4706a19bef4af2f29128b251e42c10144b89561f
pardha-nolo <113441531+pardha-nolo@users.noreply.github.com>
2022-12-21 17:56:27 +0530

    Update parser.js

--- routes/parser.js
+++ routes/parser.js
@@ -1,13 +1,19 @@
 const parser = require('transaction-sms-parser');
 
-
+function hasNumber(myString) {
+    return /\d/.test(myString);
+}
 const parse = function (request) {
     let message = request.message;
     let transactionInfo = parser.getTransactionInfo(message);
     if (transactionInfo.account.number == null) {
         let split = message.split(" ");
         for (i = 0; i < split.length; i++) {
-            if (split[i].length > 0 && (split[i].toLowerCase().includes("xx") || split[i].toLowerCase().includes("**"))) {
+            let case1 = split[i].length > 0;
+            let case2 = split[i].toLowerCase().includes("x");
+            let case3 = split[i].toLowerCase().includes("*");
+            let case4 = hasNumber(split[i]);
+            if ((case1 && case4 ) && (case2 || case3) ) {
                 let j = 4;
                 transactionInfo.account.number = split[i]
                 while(j-->0) {
@@ -19,7 +25,7 @@ const parse = function (request) {
         }
     }
     let message_lower = message.toLowerCase();
-    if(message_lower.includes("cancel") || message_lower.includes("decline") || message_lower.includes("due")){
+    if(message_lower.includes("cancel") || message_lower.includes("decline") || message_lower.includes("due") || message_lower.includes("renew")){
         transactionInfo["transactionAmount"] = 0
         transactionInfo["transactionType"] = "invalid"
     }
